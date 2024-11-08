2b33eb8b100b5a03ebd674229f57bfb2941dd9e1
Gwagsi <gwagsiglenn@gmail.com>
2024-11-08 21:14:40 +0100

    remove unused css

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,6 +1,5 @@
 
 import { Home } from './pages/home.jsx'
-import './App.css'
 import './index.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Layout } from './components/Layout.jsx'
